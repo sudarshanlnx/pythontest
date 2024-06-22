3f6017610009420797939a68aadc129a658be46c
sudarshanlnx <87026085+sudarshanlnx@users.noreply.github.com>
2024-06-22 22:55:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+import boto3
 
 app = Flask(__name__)
+
+def get_public_ip():
+    ec2_client = boto3.client('ec2')
+    response = ec2_client.describe_instances()
+    public_ip = response['Reservations'][0]['Instances'][0]['PublicIpAddress']
+    return public_ip
 
 @app.route('/')
 def hello():
     return "Hello, World!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    public_ip = get_public_ip()
+    app.run(host=public_ip, port=5000)
+
