0c01800214be9b3fa46be74046ca96d570d206b4
sudarshanlnx <87026085+sudarshanlnx@users.noreply.github.com>
2024-06-23 00:19:33 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,15 +1,1 @@
-import unittest
-from app import app
 
-class BasicTests(unittest.TestCase):
-    def setUp(self):
-        self.app = app.test_client()
-        self.app.testing = True
-
-    def test_home(self):
-        result = self.app.get('/')
-        self.assertEqual(result.status_code, 200)
-        self.assertIn(b'Hello, World!', result.data)
-
-if __name__ == "__main__":
-    unittest.main()
