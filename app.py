790b06ca07e9ed8a1a63f0341476249dd2b72a09
sudarshanlnx <87026085+sudarshanlnx@users.noreply.github.com>
2024-06-22 23:01:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,14 @@ import boto3
 app = Flask(__name__)
 
 def get_public_ip():
-    ec2_client = boto3.client('ec2')
-    response = ec2_client.describe_instances()
-    public_ip = response['Reservations'][0]['Instances'][0]['PublicIpAddress']
-    return public_ip
+    try:
+        ec2_client = boto3.client('ec2')
+        response = ec2_client.describe_instances()
+        public_ip = response['Reservations'][0]['Instances'][0]['PublicIpAddress']
+        return public_ip
+    except Exception as e:
+        print(f"Error retrieving public IP address: {e}")
+        return None
 
 @app.route('/')
 def hello():
@@ -15,5 +19,7 @@ def hello():
 
 if __name__ == "__main__":
     public_ip = get_public_ip()
-    app.run(host=public_ip, port=5000)
-
+    if public_ip:
+        app.run(host=public_ip, port=5000)
+    else:
+        print("Failed to retrieve public IP address. Exiting.")
